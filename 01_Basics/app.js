8ff1fae8c746202307aa47407a5eebdcc6ebeb12
Chinmaya <chinmayacodes@yahoo.com>
2026-02-22 06:31:24 +0000

    Basics

--- 01_Basics/app.js
+++ 01_Basics/app.js
@@ -1,16 +1,53 @@
-const heading = React.createElement('h1', {id: "heading"}, "Hello, React!");
- const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
- // Nested structure
- const parent = React.createElement("div",{id:"parent"},[
-     React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"An h1 Tag"),
-        React.createElement("h2",{},"An h2 Tag")
-    ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"An h1 Tag"),
-        React.createElement("h2",{},"An h2 Tag")
-    ])]
-);
- root.render(parent);
+// JSX - JavaScript XML {Transpiled before it reaches the JS engine/ directly transpiled to react.createElement()}
+// babel - Transpiler (JSX to JS) converts into react.createElement() and also converts ES6 to ES5
+// const heading = React.createElement("h1", { id: "heading" }, "Hello, React!");
+//  const jsxHeading = (
+//   <h1 id="heading" className="head" tabIndex="1">
+//      Hello, JSX
+//    </h1>
+//  );
+ 
+//  console.log(jsxHeading);
+
+  
+  
+//   root.render(jsxHeading);
+
+// React Components
+// Class Based Component -OLD 
+// function based component - NEW
+
+// function based component
+
+const element = <span>Hello, React Element</span>; // This is called as React Element
+
+const Heading = (
+  
+     <h1 className="head" tabIndex="1">
+      {element}
+        Hello, React Component
+    </h1>
+); // This is called as React Element
+
+const number = 1000;
+
+const HeadingComponent = () => (
+  <div id="container">
+    <h2>{number}</h2>
+    <h2>{250+150}</h2>
+    {Heading}
+    <h1 className="heading">Namaste, React Component</h1>
+    </div>
+); // This is also called as composition of components\
+// we can also call functions inside JSX
+// we can also use ternary operator and if else statements inside JSX
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # React
 The Place Where i Learn React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in c++
+- Caching => Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling => Support older Browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - Remove Unused Code
+- Different Dev and Production Bundles
