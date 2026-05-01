e74ed98f9a254f9dde7e502dc4a96fea3a2e57aa
Chinmaya-droid0 <chinmayacodes@yahoo.com>
2026-05-01 20:37:49 +0530

    Updated

---
+++ 02_Basics/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return (
+        <h2>I Love Chai</h2>
+    )
+}
+
+export default Chai
--- 02_Basics/src/main.jsx
+++ 02_Basics/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function MyApp(){
+  const userName = "Ranjan"
+  return(
+    <div>
+      <h1>Custom App | Chinmaya {userName}</h1>
+    </div>
+  )
+}
+
+// const reactElement = React.createElement(  // injected by Babble
+//   'a',
+//   {href:'https:/google.com', target:'_blank'},
+//   'Click This'
+// )
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MyApp />
   </StrictMode>,
 )
