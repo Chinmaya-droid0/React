221fb5d1614098d75cc85251fca88aa4a2551886
Chinmaya-droid0 <chinmayacodes@yahoo.com>
2026-05-06 23:05:03 +0530

    Updated

--- 05_passwordGen/src/App.jsx
+++ 05_passwordGen/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback} from 'react'
+import { useState, useCallback, useEffect, useRef} from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
@@ -10,17 +10,20 @@ function App() {
   const [char, setChar] = useState(false)
   const [pass, setPass] = useState("")
 
+  //useRef Hook
+   const passwordRef = useRef(null)
+
   const passGen = useCallback(()=>{
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
     if (num)  str+= "0123456789"
-    if (char) str+= " ! @ # $ % ^ & * ( ) - _ = + [ ] { } | : ; "
+    if (char) str+= "!@#$%^&*()-_=+[]{}|:;"
 
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <= length; i++) {
      let char = Math.floor(Math.random() * str.length + 1)
 
-     pass = str.charAt(char)
+     pass += str.charAt(char)
       
     }
   
@@ -28,10 +31,64 @@ function App() {
 
   }, [length, num, char, setPass]) 
 
+  const copyPassClip = useCallback(()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 14);
+    window.navigator.clipboard.writeText(pass)
+  },[pass])
+
+  useEffect(() => {passGen()}, [length, num, char, passGen])
+
   return (
     <>
      <h1 className="text-3xl font-bold !text-blue-500">Password Generator</h1>
-     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800'>Test</div>
+     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800'>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input 
+        type='text'
+        value={pass}
+        className='outline-none w-full py-1 px-3'
+        placeholder='Password'
+        readOnly
+        ref={passwordRef}
+        />
+        <button onClick={copyPassClip} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input
+            type='range'
+            min={6}
+            max={21}
+            value={length}
+            className='cursor-pointer'
+            onChange={(e) => {setLength(e.target.value)}}
+            />
+            <label>Length:{length}</label> 
+        </div>
+
+         <div className='flex items-center gap-x-1'>
+         <input 
+           type='checkbox'
+           defaultChecked =  {num}
+           id='numberInput'
+           onChange={()=>{setNum((prev) => !prev);}}
+         />
+         <label htmlFor='numberInput'>Numbers</label>
+         </div>
+         
+         <div className='flex items-center gap-x-1'>
+         <input 
+           type='checkbox'
+           defaultChecked =  {char}
+           id='charInput'
+           onChange={()=>{setChar((prev) => !prev);}}
+         />
+         <label htmlFor='charInput'>Characters</label>
+         </div>
+
+      </div>
+     </div>
     </>
   )
 }
